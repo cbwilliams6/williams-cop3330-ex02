b655c8b98dc4307ddcfba91864590a9908dce793
Christian Williams <christian.b.williams@gmail.com>
2021-09-11 20:24:15 -0400

    Final commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,21 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Christian Williams
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the input string? ");
+        String inputS = input.nextLine();
+        int length = inputS.length();
+
+        System.out.println(inputS + " has " + length + " characters.");
     }
 }
